1f48a7e1599eefda8fc8501e6a336d77e3f4b425
Yoshiki Kishimoto <0126yoshiki@icloud.com>
2018-04-14 01:02:53 +0900

    Initial Commit

---
+++ Quiz/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultViewController.swift
+//  Quiz
+//
+//  Created by litech on 2015/02/10.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var correctAnswer:Int = 0
+    @IBOutlet var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        resultLabel.text = "正解数は、\(correctAnswer)個！"
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
